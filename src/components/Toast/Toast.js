0229c2f90aba69d4b29e171d121f9ca89d6b0f96
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2024-05-16 18:10:01 -0500

    Enable support for keyboard and screen reader

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -31,13 +31,15 @@ function Toast({ toast, children }) {
         <Icon size={24} />
       </div>
       <p className={styles.content}>
+        <VisuallyHidden>{variant} -</VisuallyHidden>
         {children}
       </p>
       <button className={styles.closeButton}
+        aria-label="Dismiss message"
+        aria-live="off"
         onClick={() => dismissToast(id)}
       >
         <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
   );
--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,16 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [ toasts, setToasts ] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code !== 'Escape') return;
+      setToasts([]);
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+
   const dismissToast = (id) => {
     const nextToasts = toasts.filter(toast => toast.id !== id);
     setToasts(nextToasts);
